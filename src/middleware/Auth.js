660be8bb4c122c785d1f5448ba00dffd0b430329
Jordan <jlprogrammer_98@hotmail.com>
2024-02-28 17:13:52 +0000

    Authentication middleware logic

--- src/middleware/Auth.js
+++ src/middleware/Auth.js
@@ -1,5 +1,21 @@
-const express = require("express");
+const jwt = require("jsonwebtoken");
+const User = require("../model/usersSchema");
 
-const Auth = (req, res, next) => {console.log("Authenitcating");};
+const Auth = async (req, res, next) => {
+  try {
+    const token = req.header("Authorization").replace("Bearer ", "");
+    const decoded = jwt.verify(token, process.env.SECRET);
+
+    const user = await User.findOne({
+      _id: decoded.id,
+      "tokens.token": token,
+    });
+    if (!user) throw new Error();
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(401).send({ error: "Unauthorised" });
+  }
+};
 
 module.exports = Auth;
