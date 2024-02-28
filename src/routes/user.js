385fb97d34e9d707655b2f515ee1f809932cdd8d
Jordan <jlprogrammer_98@hotmail.com>
2024-02-28 16:45:53 +0000

    added GET all users route

--- src/routes/user.js
+++ src/routes/user.js
@@ -30,6 +30,14 @@ router.post("/login", async (req, res) => {
   }
 });
 
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).send(users);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
 
 router.get("/:id", async (req, res) => {
   try {
