6d76b18daf6588687382bbe40c44518add35e0fd
Jordan <jlprogrammer_98@hotmail.com>
2024-06-11 17:19:02 +0100

    initialised ReactJS app

--- src/app.js
+++ src/app.js
@@ -1,26 +1,9 @@
-const express = require("express");
-const app = express();
+function App() {
+  return (
+    <div className="App">
+      <p>My App</p>
+    </div>
+  );
+}
 
-require("dotenv").config();
-const bodyParser = require("body-parser");
-const path = require("path");
-const server_port = process.env.PORT || 3001;
-
-const public = path.join(__dirname, "../public");
-
-app.use("/public", express.static(public));
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-// parse application/json
-app.use(bodyParser.json());
-
-//connect to db first
-require('./db/mongoose');
-const userRoute = require("./routes/user");
-
-app.use("/users", userRoute);
-
-app.listen(server_port, () => {
-  console.log(`Server Listening at 127.0.0.1:${server_port}`);
-});
+export default App;
